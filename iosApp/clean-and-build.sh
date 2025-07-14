4752e94ca8089369d361b6790ed0fecaaf669819
Jordan Fisher <thementalgoose@gmail.com>
2025-07-14 20:56:45 +0100

    WIP Figuring out iOS signing

---
+++ iosApp/clean-and-build.sh
@@ -0,0 +1,15 @@
+#clean and build
+echo "CLEAN AND BUILD"
+echo "================================================================================"
+xcodebuild -workspace ./iosApp.xcworkspace/ -scheme "Production Release" -sdk iphonesimulator18.5 clean # analyze
+xcodebuild -workspace ./iosApp.xcworkspace/ -scheme "Production Release" -destination generic/platform=iOS build
+
+#archive
+echo "ARCHIVE"
+echo "================================================================================"
+xcodebuild -workspace ./iosApp.xcworkspace/ -scheme "Production Release" -sdk iphoneos -configuration AppStoreDistribution archive -archivePath $PWD/build/iosApp.xcarchive
+
+# create build ipa
+echo "CREATE IPA"
+echo "================================================================================"
+xcodebuild -exportArchive -archivePath $PWD/build/iosApp.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $PWD/build
