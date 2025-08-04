4bf92db48be9abd25246f85bac27f8b8966a26e7
Jordan Fisher <thementalgoose@gmail.com>
2025-08-04 09:23:28 +0100

    Fixes no email intent activity not found exception

--- core/device/src/androidMain/kotlin/tmg/flashback/device/usecases/OpenEmailUseCase.android.kt
+++ core/device/src/androidMain/kotlin/tmg/flashback/device/usecases/OpenEmailUseCase.android.kt
@@ -1,9 +1,15 @@
 package tmg.flashback.device.usecases
 
+import android.content.ActivityNotFoundException
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
+import android.os.Build
+import android.widget.Toast
 import org.koin.java.KoinJavaComponent
 
 
@@ -18,6 +24,20 @@ actual class OpenEmailUseCaseImpl actual constructor(): OpenEmailUseCase {
         emailIntent.data = Uri.parse("mailto:$email")
         emailIntent.flags = FLAG_ACTIVITY_NEW_TASK
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Flashback")
-        getApplicationContext().startActivity(emailIntent)
+        try {
+            getApplicationContext().startActivity(emailIntent)
+        } catch (e: ActivityNotFoundException) {
+            copyToClipboard(getApplicationContext(), email)
+        }
+    }
+
+
+    private fun copyToClipboard(context: Context, url: String): Boolean {
+        val clipboardManager = (context.getSystemService(CLIPBOARD_SERVICE) as? ClipboardManager) ?: return false
+        clipboardManager.setPrimaryClip(ClipData.newPlainText("", url))
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+            Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }
--- core/device/src/androidMain/kotlin/tmg/flashback/device/usecases/OpenStorePageUseCase.android.kt
+++ core/device/src/androidMain/kotlin/tmg/flashback/device/usecases/OpenStorePageUseCase.android.kt
@@ -1,10 +1,15 @@
 package tmg.flashback.device.usecases
 
 import android.content.ActivityNotFoundException
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
+import android.os.Build
+import android.widget.Toast
 import org.koin.java.KoinJavaComponent
 import tmg.flashback.device.PLAY_STORE_LINK
 
@@ -21,6 +26,18 @@ actual class OpenStorePageUseCaseImpl actual constructor(): OpenStorePageUseCase
                     flags = FLAG_ACTIVITY_NEW_TASK
                 }
             )
-        } catch (e: ActivityNotFoundException) { }
+        } catch (e: ActivityNotFoundException) {
+            copyToClipboard(getApplicationContext(), PLAY_STORE_LINK)
+        }
+    }
+
+
+    private fun copyToClipboard(context: Context, url: String): Boolean {
+        val clipboardManager = (context.getSystemService(CLIPBOARD_SERVICE) as? ClipboardManager) ?: return false
+        clipboardManager.setPrimaryClip(ClipData.newPlainText("", url))
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+            Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }
