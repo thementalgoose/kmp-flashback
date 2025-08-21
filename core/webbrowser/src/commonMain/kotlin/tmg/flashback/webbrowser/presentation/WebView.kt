31d3d5890f5dbc49ece6fd176723a33530af5266
Jordan Fisher <thementalgoose@gmail.com>
2025-08-21 09:08:50 +0100

    Migrates material to material3 webview

--- core/webbrowser/src/commonMain/kotlin/tmg/flashback/webbrowser/presentation/WebView.kt
+++ core/webbrowser/src/commonMain/kotlin/tmg/flashback/webbrowser/presentation/WebView.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
