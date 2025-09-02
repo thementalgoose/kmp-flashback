4ff64e52ebcfff22e5e9875f12cb007afbc50e6b
Jordan Fisher <thementalgoose@gmail.com>
2025-09-02 20:54:30 +0100

    Sets OKHttp client via. preconfigured engine

--- data/network/rss/src/androidMain/kotlin/tmg/flashback/network/rss/client/KtorClient.android.kt
+++ data/network/rss/src/androidMain/kotlin/tmg/flashback/network/rss/client/KtorClient.android.kt
@@ -4,20 +4,26 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.xml.xml
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import tmg.flashback.infrastructure.device.Device
 import tmg.flashback.infrastructure.network.DnsUtils
 
 actual val KtorClient: HttpClient by lazy {
+
+    val okhttpClient = OkHttpClient.Builder().build()
+    val dns = DnsUtils.getDns(okhttpClient)
+
     HttpClient(OkHttp) {
         // default validation to throw exceptions for non-2xx responses
         expectSuccess = true
 
         engine {
-            config {
-                dns(DnsUtils.dns)
-            }
-            // add logging interceptor
+            preconfigured = okhttpClient
+                .newBuilder()
+                .dns(dns)
+                .build()
+
             if (Device.isDebug) {
                 addInterceptor(
                     HttpLoggingInterceptor().apply {
--- infrastructure/src/androidMain/kotlin/tmg/flashback/infrastructure/network/DnsUtils.kt
+++ infrastructure/src/androidMain/kotlin/tmg/flashback/infrastructure/network/DnsUtils.kt
@@ -1,11 +1,13 @@
 package tmg.flashback.infrastructure.network
 
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
 import okhttp3.dnsoverhttps.DnsOverHttps
 import java.net.InetAddress
 
 object DnsUtils {
-    val dns = DnsOverHttps.Builder()
+    fun getDns(client: OkHttpClient) = DnsOverHttps.Builder()
+        .client(client)
         .url("https://1.1.1.1/dns-query".toHttpUrl())
         .includeIPv6(false)
         .bootstrapDnsHosts(
