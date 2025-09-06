5567e9ab550fa294c823c5778d5bdf9117ddb817
Jordan Fisher <thementalgoose@gmail.com>
2025-09-06 16:42:57 +0100

    Fixes desktop compiling

--- composeApp/src/desktopMain/kotlin/tmg/flashback/firebase/FirebaseMessagingService.native.kt
+++ composeApp/src/desktopMain/kotlin/tmg/flashback/firebase/FirebaseMessagingService.native.kt
@@ -3,11 +3,12 @@ package tmg.flashback.firebase
 import tmg.flashback.notifications.firebase.FirebaseMessagingService
 
 internal actual class FirebaseMessagingServiceImpl actual constructor() : FirebaseMessagingService {
-    actual override suspend fun subscribeToTopic(topicId: String) {
 
+    actual override suspend fun subscribeToTopic(topicId: String): Boolean {
+        return false
     }
 
-    actual override suspend fun unsubscribeFromTopic(topicId: String) {
-
+    actual override suspend fun unsubscribeFromTopic(topicId: String): Boolean {
+        return false
     }
 }
