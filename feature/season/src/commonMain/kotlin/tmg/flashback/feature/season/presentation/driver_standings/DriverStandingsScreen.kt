fe0a2fbfcb930ff1a055766eceb30d990043f43e
Jordan Fisher <thementalgoose@gmail.com>
2025-12-08 17:45:03 +0000

    Updates championship highlight color

--- feature/season/src/commonMain/kotlin/tmg/flashback/feature/season/presentation/driver_standings/DriverStandingsScreen.kt
+++ feature/season/src/commonMain/kotlin/tmg/flashback/feature/season/presentation/driver_standings/DriverStandingsScreen.kt
@@ -147,7 +147,7 @@ private fun DriverStandings(
     modifier: Modifier = Modifier,
 ) {
     val backgroundColor = when {
-        model.inProgressContent == null && model.championshipPosition == 1 -> AppTheme.colors.f1Podium1.copy(alpha = 0.2f)
+        model.inProgressContent == null && model.championshipPosition == 1 -> AppTheme.colors.f1ChampionshipHighlight
         else -> Transparent
     }
     val constructorColor =
