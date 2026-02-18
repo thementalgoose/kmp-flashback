5953c2d9db6c1ee4b94c566fa60c5ae0076e20f2
Jordan Fisher <thementalgoose@gmail.com>
2026-02-18 21:16:27 +0000

    Adds all screens + screen state configuration

--- presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/Screen.kt
+++ presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/Screen.kt
@@ -18,6 +18,13 @@ sealed interface Screen {
     data object Circuits: Screen
 
     @Serializable
+    data class Weekend(
+        val season: Int,
+        val round: Int,
+        val raceName: String,
+    ): Screen
+
+    @Serializable
     data class Circuit(
         val id: String,
         val name: String,
@@ -26,29 +33,67 @@ sealed interface Screen {
     }
 
     @Serializable
-    data class Constructor(
+    data class Driver(
+        val season: Int,
         val id: String,
         val name: String
-    ): Screen {
-        companion object
-    }
+    ): Screen
 
     @Serializable
-    data class Driver(
+    data class Team(
+        val season: Int,
         val id: String,
         val name: String
-    ): Screen {
-        companion object
-    }
+    ): Screen
+
+    @Serializable
+    data class DriverComparison(
+        val season: Int
+    ): Screen
 
     @Serializable
     data object Rss: Screen
+
+    @Serializable
+    data class Webpage(
+        val url: String
+    ): Screen
 
     @Serializable
     data object ReactionGame: Screen
 
     @Serializable
     data object Settings: Screen
+
+    @Serializable
+    data object SettingsDarkMode: Screen
+
+    @Serializable
+    data object SettingsTheme: Screen
+
+    @Serializable
+    data object SettingsLayout: Screen
+
+    @Serializable
+    data object SettingsWeather: Screen
+
+    @Serializable
+    data object SettingsRssConfigure: Screen
+
+    @Serializable
+    data object SettingsInAppBrowser: Screen
+
+    @Serializable
+    data object SettingsNotificationResults: Screen
+
+    @Serializable
+    data object SettingsNotificationUpcoming: Screen
+
+    @Serializable
+    data object SettingsWidgets: Screen
+
+    @Serializable
+    data object SettingsPrivacy: Screen
 
     @Serializable
     data object PrivacyPolicy: Screen
--- presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/ScreenStateConfiguration.kt
+++ presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/ScreenStateConfiguration.kt
@@ -1,4 +1,42 @@
 package tmg.flashback.navigation
 
-class ScreenStateConfiguration {
+import androidx.savedstate.serialization.SavedStateConfiguration
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+val saveStateConfiguration = SavedStateConfiguration {
+    serializersModule = SerializersModule {
+        polymorphic(Screen::class) {
+            subclass(Screen.Calendar::class, Screen.Calendar.serializer())
+            subclass(Screen.DriverStandings::class, Screen.DriverStandings.serializer())
+            subclass(Screen.TeamStandings::class, Screen.TeamStandings.serializer())
+            subclass(Screen.Circuits::class, Screen.Circuits.serializer())
+
+            subclass(Screen.Weekend::class, Screen.Weekend.serializer())
+            subclass(Screen.Circuit::class, Screen.Circuit.serializer())
+            subclass(Screen.Driver::class, Screen.Driver.serializer())
+            subclass(Screen.Team::class, Screen.Team.serializer())
+            subclass(Screen.DriverComparison::class, Screen.DriverComparison.serializer())
+
+            subclass(Screen.Rss::class, Screen.Rss.serializer())
+            subclass(Screen.Webpage::class, Screen.Webpage.serializer())
+
+            subclass(Screen.ReactionGame::class, Screen.ReactionGame.serializer())
+
+            subclass(Screen.Settings::class, Screen.Settings.serializer())
+            subclass(Screen.SettingsDarkMode::class, Screen.SettingsDarkMode.serializer())
+            subclass(Screen.SettingsTheme::class, Screen.SettingsTheme.serializer())
+            subclass(Screen.SettingsLayout::class, Screen.SettingsLayout.serializer())
+            subclass(Screen.SettingsWeather::class, Screen.SettingsWeather.serializer())
+            subclass(Screen.SettingsRssConfigure::class, Screen.SettingsRssConfigure.serializer())
+            subclass(Screen.SettingsInAppBrowser::class, Screen.SettingsInAppBrowser.serializer())
+            subclass(Screen.SettingsNotificationResults::class, Screen.SettingsNotificationResults.serializer())
+            subclass(Screen.SettingsNotificationUpcoming::class, Screen.SettingsNotificationUpcoming.serializer())
+            subclass(Screen.SettingsWidgets::class, Screen.SettingsWidgets.serializer())
+            subclass(Screen.SettingsPrivacy::class, Screen.SettingsPrivacy.serializer())
+            subclass(Screen.PrivacyPolicy::class, Screen.PrivacyPolicy.serializer())
+
+            subclass(Screen.About::class, Screen.About.serializer())
+        }
+    }
 }
