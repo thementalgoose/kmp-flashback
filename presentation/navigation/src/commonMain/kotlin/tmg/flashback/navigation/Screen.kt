2e484ea290aa1300f0fb30ec647ed2f9d9020820
Jordan Fisher <thementalgoose@gmail.com>
2026-02-18 21:37:19 +0000

    Adds ListDetailScene

--- presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/Screen.kt
+++ presentation/navigation/src/commonMain/kotlin/tmg/flashback/navigation/Screen.kt
@@ -1,9 +1,10 @@
 package tmg.flashback.navigation
 
+import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface Screen {
+sealed interface Screen: NavKey {
 
     @Serializable
     data object Calendar: Screen
