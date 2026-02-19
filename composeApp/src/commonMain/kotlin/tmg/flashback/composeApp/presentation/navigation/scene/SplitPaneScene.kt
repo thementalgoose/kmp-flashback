cba7db9de88763b053c468e1a90e372cb5cc4710
Jordan Fisher <thementalgoose@gmail.com>
2026-02-19 19:58:58 +0000

    Changes SplitPaneScene to add enterance animation

--- composeApp/src/commonMain/kotlin/tmg/flashback/composeApp/presentation/navigation/scene/SplitPaneScene.kt
+++ composeApp/src/commonMain/kotlin/tmg/flashback/composeApp/presentation/navigation/scene/SplitPaneScene.kt
@@ -1,12 +1,9 @@
 package tmg.flashback.composeApp.presentation.navigation.scene
 
-
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
@@ -22,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.scene.Scene
@@ -33,7 +29,8 @@ import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOW
 import tmg.flashback.composeApp.presentation.navigation.scene.SplitPaneScene.Companion.DETAIL_KEY
 import tmg.flashback.composeApp.presentation.navigation.scene.SplitPaneScene.Companion.LIST_KEY
 import tmg.flashback.style.AppTheme
-import tmg.flashback.ui.components.fade.Fade
+
+private val spacingPadding = 12.dp
 
 class SplitPaneScene<T : Any>(
     override val key: Any,
@@ -44,15 +41,25 @@ class SplitPaneScene<T : Any>(
     override val entries: List<NavEntry<T>> = listOfNotNull(listEntry, detailEntry)
     override val content: @Composable (() -> Unit) = {
         val safeDrawingInsets = WindowInsets.safeDrawing.asPaddingValues()
-
-        Row(modifier = Modifier
+        BoxWithConstraints(modifier = Modifier
             .fillMaxSize()
             .padding(safeDrawingInsets)
             .padding(end = AppTheme.dimens.medium)
             .consumeWindowInsets(WindowInsets.safeDrawing),
-            horizontalArrangement = Arrangement.spacedBy(AppTheme.dimens.medium)
         ) {
-            Box(Modifier.weight(1f)) {
+            val listWidth = animateDpAsState(
+                targetValue = when {
+                    detailEntry == null -> maxWidth
+                    else -> (maxWidth / 2f) - spacingPadding
+                },
+                label = "listWidth"
+            )
+            val detailWidth = (maxWidth / 2f)
+
+            Box(Modifier
+                .width(listWidth.value)
+                .fillMaxHeight()
+            ) {
                 Box(Modifier
                     .fillMaxSize()
                     .clip(RoundedCornerShape(AppTheme.dimens.radiusMedium))
@@ -62,7 +69,11 @@ class SplitPaneScene<T : Any>(
                 }
             }
             if (detailEntry != null) {
-                Box(Modifier.weight(1f)) {
+                Box(Modifier
+                    .width(detailWidth)
+                    .fillMaxHeight()
+                    .offset(listWidth.value + spacingPadding)
+                ) {
                     Box(Modifier
                         .fillMaxSize()
                         .clip(RoundedCornerShape(AppTheme.dimens.radiusMedium))
