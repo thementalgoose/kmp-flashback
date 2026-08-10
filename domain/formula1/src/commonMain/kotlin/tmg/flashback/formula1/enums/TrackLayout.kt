63de0a5515f0d475b70ca7d9f6d305260ac60fa0
Jordan Fisher <thementalgoose@gmail.com>
2026-08-10 09:27:15 +0100

    Adds Barcelona and Austria

--- domain/formula1/src/commonMain/kotlin/tmg/flashback/formula1/enums/TrackLayout.kt
+++ domain/formula1/src/commonMain/kotlin/tmg/flashback/formula1/enums/TrackLayout.kt
@@ -80,7 +80,8 @@ enum class TrackLayout(
     ),
     CATALUNYA(
         circuitId = "catalunya",
-        icon = Res.drawable.circuit_catalunya
+        icon = Res.drawable.circuit_catalunya,
+        breakdown = TrackBreakdown.CATALUNYA
     ),
     CATALUNYA_1991_2022(
         circuitId = "catalunya",
@@ -283,7 +284,8 @@ enum class TrackLayout(
     ),
     RED_BULL_RING(
         circuitId = "red_bull_ring",
-        icon = Res.drawable.circuit_red_bull_ring
+        icon = Res.drawable.circuit_red_bull_ring,
+        breakdown = TrackBreakdown.RED_BULL_RING
     ),
     REIMS(
         circuitId = "reims",
