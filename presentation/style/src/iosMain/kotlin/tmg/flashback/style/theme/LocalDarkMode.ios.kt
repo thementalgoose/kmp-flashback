29b828dd01706581b65c437934ee2be05d3bef3c
Jordan Fisher <thementalgoose@gmail.com>
2026-08-28 16:53:58 +0100

    Fixes iOS theme references

--- presentation/style/src/iosMain/kotlin/tmg/flashback/style/theme/LocalDarkMode.ios.kt
+++ presentation/style/src/iosMain/kotlin/tmg/flashback/style/theme/LocalDarkMode.ios.kt
@@ -2,23 +2,18 @@ package tmg.flashback.style.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidedValue
-import androidx.compose.ui.InternalComposeUiApi
-import androidx.compose.ui.LocalSystemTheme
-import androidx.compose.ui.SystemTheme
+import androidx.compose.runtime.compositionLocalOf
+import org.jetbrains.skiko.SystemTheme
+import org.jetbrains.skiko.currentSystemTheme
 
-@OptIn(InternalComposeUiApi::class)
+private val LocalIosLightMode = compositionLocalOf<Boolean?> { null }
+
 actual object LocalDarkMode {
     actual val current: Boolean
-        @Composable get() = LocalSystemTheme.current == SystemTheme.Light
+        @Composable get() = LocalIosLightMode.current ?: (currentSystemTheme != SystemTheme.DARK)
 
     @Composable
     actual infix fun provides(value: Boolean?): ProvidedValue<*> {
-        val new = when(value) {
-            true -> SystemTheme.Light
-            false -> SystemTheme.Dark
-            null -> LocalSystemTheme.current
-        }
-
-        return LocalSystemTheme.provides(new)
+        return LocalIosLightMode.provides(value)
     }
 }
