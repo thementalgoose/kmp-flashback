faf25004dd5587ef0ae4f476f2d5474fb22ef439
Jordan Fisher <thementalgoose@gmail.com>
2026-08-29 11:40:04 +0100

    Adds lineup network models

---
+++ data/network/flashback/src/commonMain/kotlin/tmg/flashback/flashbackapi/api/models/lineup/Lineup.kt
@@ -0,0 +1,16 @@
+package tmg.flashback.flashbackapi.api.models.lineup
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import tmg.flashback.flashbackapi.api.models.constructors.Constructor
+import tmg.flashback.flashbackapi.api.models.drivers.Driver
+
+@Serializable
+data class Lineup(
+    @SerialName("drivers")
+    val drivers: Map<String, Driver>,
+    @SerialName("constructors")
+    val constructors: Map<String, Constructor>,
+    @SerialName("lineup")
+    val lineup: Map<String, LineupSeason>
+)
