2293b98bbeacf4b8b28e8443031bb5f51cf416d8
Jordan Fisher <thementalgoose@gmail.com>
2026-08-30 00:02:21 +0100

    Fixes unit test

--- data/flashback/src/commonTest/kotlin/tmg/flashback/data/repo/mappers/app/LineupMapperTest.kt
+++ data/flashback/src/commonTest/kotlin/tmg/flashback/data/repo/mappers/app/LineupMapperTest.kt
@@ -2,9 +2,12 @@ package tmg.flashback.data.repo.mappers.app
 
 import tmg.flashback.data.repo.fakes.fakeConstructorDataMapper
 import tmg.flashback.data.repo.fakes.fakeDriverDataMapper
+import tmg.flashback.formula1.model.Constructor
+import tmg.flashback.formula1.model.Driver
+import tmg.flashback.formula1.model.LineupSeason
+import tmg.flashback.formula1.model.model
 import tmg.flashback.persistence.flashback.models.lineup.LineupWithDrivers
 import tmg.flashback.persistence.flashback.models.lineup.model
-import tmg.flashback.formula1.model.model
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -24,7 +27,10 @@ internal class LineupMapperTest {
         initUnderTest()
 
         val inputModel = LineupWithDrivers.model()
-        val expected = tmg.flashback.formula1.model.LineupSeason.model()
+        val expected = LineupSeason(
+            season = 2020,
+            driversToConstructors = mapOf(Driver.model() to Constructor.model())
+        )
 
         assertEquals(listOf(expected), sut.mapLineup(listOf(inputModel)))
     }
