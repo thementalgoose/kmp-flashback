f0bcf4d71afb885bc15d94efd9b3c7767e802974
Jordan Fisher <thementalgoose@gmail.com>
2026-09-02 23:14:28 +0100

    Adds worker.js

--- data/persistence/flashback/src/wasmJsMain/kotlin/tmg/flashback/persistence/flashback/FlashbackDatabase.wasmJs.kt
+++ data/persistence/flashback/src/wasmJsMain/kotlin/tmg/flashback/persistence/flashback/FlashbackDatabase.wasmJs.kt
@@ -5,13 +5,13 @@ import androidx.room3.RoomDatabase
 import androidx.sqlite.SQLiteDriver
 import androidx.sqlite.driver.web.WebWorkerSQLiteDriver
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import org.w3c.dom.Worker
 
 actual class FlashbackDatabaseFactory {
     actual fun createDatabase(): RoomDatabase.Builder<FlashbackDatabase> =
         Room.inMemoryDatabaseBuilder()
 
-    actual fun getSQLiteDriver(): SQLiteDriver = WebWorkerSQLiteDriver()
+    actual fun getSQLiteDriver(): SQLiteDriver = WebWorkerSQLiteDriver(Worker("worker.js"))
 
     actual fun getDispatcher(): CoroutineDispatcher? = null
 }
