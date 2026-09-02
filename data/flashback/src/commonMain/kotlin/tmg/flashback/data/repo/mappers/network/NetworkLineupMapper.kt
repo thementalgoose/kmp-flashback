7d2f7ace32afe98fe30b59a08c283cb1eb3fdccd
Jordan Fisher <thementalgoose@gmail.com>
2026-09-02 22:03:07 +0100

    Removes log statement

--- data/flashback/src/commonMain/kotlin/tmg/flashback/data/repo/mappers/network/NetworkLineupMapper.kt
+++ data/flashback/src/commonMain/kotlin/tmg/flashback/data/repo/mappers/network/NetworkLineupMapper.kt
@@ -1,13 +1,11 @@
 package tmg.flashback.data.repo.mappers.network
 
-import tmg.flashback.infrastructure.log.logDebug
 import tmg.flashback.persistence.flashback.models.lineup.Lineup
 
 class NetworkLineupMapper {
 
     private fun mapLineup(model: tmg.flashback.flashbackapi.api.models.lineup.LineupSeason): List<Lineup> {
         return model.drivers.map { (driverId, constructorId) ->
-            logDebug("Saving Lineup $model")
             Lineup(
                 constructorId = constructorId,
                 driverId = driverId,
