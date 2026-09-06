41634083a4b34582081be6302f3377139a9ca227
Jordan Fisher <thementalgoose@gmail.com>
2026-09-06 11:02:29 +0100

    Adds in memory cache for shared prefs

--- core/preferences/src/desktopMain/kotlin/tmg/flashback/preferences/service/StorageService.native.kt
+++ core/preferences/src/desktopMain/kotlin/tmg/flashback/preferences/service/StorageService.native.kt
@@ -1,40 +1,44 @@
 package tmg.flashback.preferences.service
 
 actual class StorageService {
-    actual fun save(key: String, value: Int) {
 
+    val memoryCache: MutableMap<String, Any> = mutableMapOf()
+
+    actual fun save(key: String, value: Int) {
+        memoryCache[key] = value
     }
     actual fun save(key: String, value: String) {
-
+        memoryCache[key] = value
     }
     actual fun save(key: String, value: Long) {
-
+        memoryCache[key] = value
     }
     actual fun save(key: String, value: Float) {
-
+        memoryCache[key] = value
     }
     actual fun save(key: String, value: Boolean) {
-
+        memoryCache[key] = value
     }
     actual fun save(key: String, value: Set<String>) {
-
+        memoryCache[key] = value
     }
     actual fun getInt(key: String, value: Int): Int {
-        return value
+        return (memoryCache[key] as? Int) ?: value
     }
     actual fun getString(key: String, value: String?): String? {
-        return value
+        return (memoryCache[key] as? String) ?:value
     }
     actual fun getLong(key: String, value: Long): Long {
-        return value
+        return (memoryCache[key] as? Long) ?:value
     }
     actual fun getFloat(key: String, value: Float): Float {
-        return value
+        return (memoryCache[key] as? Float) ?:value
     }
     actual fun getBoolean(key: String, value: Boolean): Boolean {
-        return value
+        return (memoryCache[key] as? Boolean) ?:value
     }
     actual fun getSet(key: String, value: Set<String>): MutableSet<String> {
-        return value.toMutableSet()
+        val result = memoryCache[key]
+        return (memoryCache[key] as? Set<*>)?.map { it.toString() }?.toMutableSet() ?: value.toMutableSet()
     }
 }
